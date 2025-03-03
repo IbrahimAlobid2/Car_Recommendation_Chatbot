41bace9e90856e182578bcd36c06e04922ebc088
IbrahimAlobid <ibrahim.alobid44@gmail.com>
2025-03-03 17:26:03 +0300

    create controller modelar

---
+++ src/controllers/__init__.py
@@ -0,0 +1,5 @@
+from .DataProcessController import DataProcessController
+from .RAGController import RAGController
+from .SQL_AgentController import SQL_AgentController
+from .ChatbotController import ChatbotController
+
